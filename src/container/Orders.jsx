7d0e52ec8c413f93fb2ab73776bac4ce872cf501
Braden-Preston <bpfilmsinc@gmail.com>
2018-11-24 16:34:46 -0600

    Flex Test

--- src/container/Orders.jsx
+++ src/container/Orders.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { AppBar, Toolbar, IconButton, Typography, InputBase, Paper } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import { fade } from '@material-ui/core/styles/colorManipulator';
+import ColorAvatar from '../presentation/ColorAvatar';
 
 const styles = theme => ({
     root: {
         position: 'relative',
         width: '100vw',
         height: '100vh',
-        // border: '2px solid lime !important'
+        // overflow: 'hidden',
+        // border: '2px solid yellow !important'
     },
     grow: {
         flexGrow: 1,
@@ -72,27 +74,47 @@ const styles = theme => ({
             },
         },
     },
-    appBar: {
-        position: 'relative',
-        // border: '2px solid lime !important'
+    flex: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        // '& div': {
+        //     display: 'flex',
+        //     flexDirection: 'column',
+        //     justifyContent: 'center',
+        // },
+        // overflowY: 'hidden',
+        // overflowX: 'scroll',
     },
-    sliceBox: {
-        // position: 'relative',
-        height: 'inherit',
+    appBar: {
+        // maxHeight: 80,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: 56,
+        // border: '2px solid lime !important',
+    },
+    canvas: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        height: 250,
+        background: 'rgba(0,140,255,0.7)',
         // border: '2px solid magenta !important',
     },
-    header: {
-        height: 250,
-        background: 'rgba(0,140,255,0.7)'
-    },
     slice: {
-        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        flex: 1,
+        overflowY: 'scroll',
+        overflowX: 'hidden',
         background: theme.palette.background.default,
         background: '#f7f7f7',
+        // border: '2px solid red !important',
     },
     paper: {
-        boxSizing: 'border-box',
-        position: 'absolute',
+        position: 'realtive',
         padding: theme.spacing.unit * 2,
         margin: theme.spacing.unit * 2,
         width: 'calc(100vw - 32px)',
@@ -104,8 +126,11 @@ class Orders extends React.Component {
         const { classes } = this.props
         return (
             <div id="Orders" className={classes.root}>
-                <ActionBar classes={classes} />
-                <SliceContainer classes={classes} />
+                <div id="Flex" className={classes.flex}>
+                    <ActionBar classes={classes} />
+                    <Canvas classes={classes} />
+                    <SliceContainer classes={classes} />
+                </div>
             </div>
         )
     }
@@ -114,46 +139,49 @@ class Orders extends React.Component {
 export default withStyles(styles)(Orders);
 
 const ActionBar = ({ classes }) => (
-    <AppBar position="static" color="secondary" className={classes.appBar}>
-        <Toolbar className={classes.toolbar}>
-            <div className={classes.grow} />
-            <div className={classes.search}>
-                <div className={classes.searchIcon}> <SearchIcon /> </div>
-                <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput, }} />
-            </div>
-            <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
-                <MenuIcon />
-            </IconButton>
-        </Toolbar>
-    </AppBar>
-)
-
-const SliceContainer = ({ classes }) => (
-    <div className={classes.sliceBox}>
-        <SvgHeader classes={classes} />
-        <Slice classes={classes} />
+    <div id="AppBar" className={classes.appBar}>
+        <AppBar position="static" color="secondary">
+            <Toolbar className={classes.toolbar}>
+                <div className={classes.grow} />
+                <div className={classes.search}>
+                    <div className={classes.searchIcon}> <SearchIcon /> </div>
+                    <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput, }} />
+                </div>
+                <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+                    <MenuIcon />
+                </IconButton>
+            </Toolbar>
+        </AppBar>
     </div>
 )
 
-const SvgHeader = ({ classes }) => (
-    <div className={classes.header}>
+const Canvas = ({ classes }) => (
+    <div className={classes.canvas}>
         <Typography variant="h4" color="textSecondary">Orders</Typography>
     </div>
 )
 
-const Slice = ({ classes }) => (
+const SliceContainer = ({ classes }) => (
     <div className={classes.slice}>
-        <PanelGroup classes={classes} />
+        <SliceGroup classes={classes} />
+        <SliceGroup classes={classes} />
+        <SliceGroup classes={classes} />
+        <SliceGroup classes={classes} />
+        <SliceGroup classes={classes} />
     </div>
 )
 
-const PanelGroup = ({ classes }) => (
-    <PanelSet classes={classes} />
+const SliceGroup = ({ classes }) => (
+    <Fragment>
+        <p>This Week</p>
+        <Slice classes={classes} />
+    </Fragment>
 )
 
-const PanelSet = ({ classes }) => (
+const Slice = ({ classes }) => (
     <Paper className={classes.paper} >
-        <p>Order 1</p> <hr />
+        <ColorAvatar initials="BP" />
+        <p> Order 1 </p> <hr />
         <p>Order 2</p> <hr />
         <p>Order 3</p>
     </Paper >
